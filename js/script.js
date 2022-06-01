db0dfd132bc8c38632023d5315498351191f27e9
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-06-02 00:10:30 +0530

    Disable scroll wheel on nav open

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function closeNav() {
     document.querySelector(".overlay").style.width = "0";
+    document.querySelector("body").classList.remove("D-scroll");
 }
 
 function openNav() {
     document.querySelector(".overlay").style.width = "100%"
+    document.querySelector("body").classList.add("D-scroll");
 }
